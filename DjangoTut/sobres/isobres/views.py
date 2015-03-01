59d0854a4dce3718571a5d133ccf4e3a31bb7636
Carles Mateu <carlesm@carlesm.com>
2015-03-01 12:23:13 +0100

    Created first view (homepage)

--- DjangoTut/sobres/isobres/views.py
+++ DjangoTut/sobres/isobres/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def mainpage(request):
+    output = '''
+        <html>
+        <head><title>%s</title></head>
+        <body>
+        <h1>%s</h1><p>%s</p>
+        </body>
+        </html>
+        ''' % ( 'Sobres aPP',
+        'Welcome to the Sobres aPPlication',
+        'Managing non legal funding since 2013')
+    return HttpResponse(output)
+
--- DjangoTut/sobres/sobres/urls.py
+++ DjangoTut/sobres/sobres/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
+from isobres.views import *
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sobres.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', mainpage, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
