48f32af223c0a61372be6149dac12184e2703210
Carles Mateu <carlesm@carlesm.com>
2015-03-01 12:31:47 +0100

    Model Sobre

--- DjangoTut/sobres/isobres/models.py
+++ DjangoTut/sobres/isobres/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Sobre(models.Model):
+    date = models.DateTimeField()
+    amount = models.IntegerField()
+    concept = models.TextField(max_length=100)
