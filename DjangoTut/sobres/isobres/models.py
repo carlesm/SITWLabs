f7574099ce356133c02ffdd79dc29485473bd47c
Carles Mateu <carlesm@carlesm.com>
2015-03-01 13:19:50 +0100

    Fixed order

--- DjangoTut/sobres/isobres/models.py
+++ DjangoTut/sobres/isobres/models.py
@@ -4,14 +4,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Donor(models.Model):
+    name = models.CharField(max_length=40)
+
+
 class Sobre(models.Model):
     date = models.DateTimeField()
     amount = models.IntegerField()
     concept = models.TextField(max_length=100)
     donor = models.ForeignKey(Donor)
     user = models.ForeignKey(User)
-
-
-class Donor(models.Model):
-    name = models.CharField(max_length=40)
 
