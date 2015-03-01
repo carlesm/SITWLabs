1683ae0f51c9ac2d6128758a44ebc7d4f404646c
Carles Mateu <carlesm@carlesm.com>
2015-03-01 08:45:34 +0100

    JSON version, still too long

---
+++ BasicWeb/getweather-json.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+'''
+Get weather from weather underground
+Created on 11/22/2014
+
+Using XML as format, minidom
+
+@author: carlesm
+'''
+
+import sys
+import requests
+import json
+
+api_key = None  # If assigned won't read argv[1]
+
+
+
+class WeatherClient(object):
+
+    """Will access wunderground to gather weather information
+
+    Provides access to wunderground API
+    (http://www.wunderground.com/weather/api)
+
+    Provides methods:
+        almanac
+    """
+
+    url_base = 'http://api.wunderground.com/api/'
+    url_services = {
+        "almanac": "/almanac/q/CA/"
+    }
+
+    def __init__(self, apikey):
+        super(WeatherClient, self).__init__()
+        self.api_key = apikey
+
+    def getNodeText(self, node):
+        nodelist = node.childNodes
+        result = []
+        for node in nodelist:
+            if node.nodeType == node.TEXT_NODE:
+                result.append(node.data)
+        return ''.join(result)
+
+    def almanac(self, location):
+        """
+        Accesses wunderground almanac information for the given location
+        """
+        resp_format = "json"
+        url = WeatherClient.url_base + api_key + \
+            WeatherClient.url_services[
+                "almanac"] + location + "." + resp_format
+        r = requests.get(url)
+
+        return_response = {}
+
+
+        jsondata = json.loads(r.text)
+        return_response["high"] = {}
+        return_response["high"]["normal"] = \
+            jsondata['almanac']['temp_high']['normal']['C']
+        return_response["high"]["record"] = \
+            jsondata['almanac']['temp_high']['record']['C']
+
+        return_response["high"]["year"] = \
+            jsondata['almanac']['temp_high']['recordyear']
+
+        return_response["low"] = {}
+        return_response["low"]["normal"] = \
+            jsondata['almanac']['temp_low']['normal']['C']
+        return_response["low"]["record"] = \
+            jsondata['almanac']['temp_low']['record']['C']
+        return_response["low"]["year"] = \
+            jsondata['almanac']['temp_low']['recordyear']
+
+
+        return return_response
+
+
+def print_almanac(almanac):
+    """
+    Prints an almanac received as a dict
+    """
+    print "High Temperatures:"
+    print "Average on this date", almanac["high"]["normal"]
+    print "Record on this date %s (%s) " % \
+        (almanac["high"]["record"],
+            almanac["high"]["year"])
+    print "Low Temperatures:"
+    print "Average on this date", almanac["low"]["normal"]
+    print "Record on this date %s (%s) " % \
+        (almanac["low"]["record"],
+            almanac["low"]["year"])
+
+
+if __name__ == "__main__":
+    if not api_key:
+        try:
+            api_key = sys.argv[1]
+        except IndexError:
+            print "Must provide api key in code or cmdline arg"
+
+    weatherclient = WeatherClient(api_key)
+    print_almanac(weatherclient.almanac("San_Francisco"))
