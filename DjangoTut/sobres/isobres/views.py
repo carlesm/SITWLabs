77b17df778cb72370deda94e12c11cdc6eeaf665
Carles Mateu <carlesm@carlesm.com>
2015-03-01 13:25:56 +0100

    Added render via template

--- DjangoTut/sobres/isobres/views.py
+++ DjangoTut/sobres/isobres/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 
 # Create your views here.
 def mainpage(request):
-    output = '''
-        <html>
-        <head><title>%s</title></head>
-        <body>
-        <h1>%s</h1><p>%s</p>
-        </body>
-        </html>
-        ''' % ( 'Sobres aPP',
-        'Welcome to the Sobres aPPlication',
-        'Managing non legal funding since 2013')
+    template = get_template('mainpage.html')
+    variables = Context({
+        'titlehead': 'Sobres aPP',
+        'pagetitle': 'Welcome to the Sobres aPPlication',
+        'contentbody': 'Managing non legal funding since 2013'
+        })
+    output = template.render(variables)
     return HttpResponse(output)
 
