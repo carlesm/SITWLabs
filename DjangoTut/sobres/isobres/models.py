f84666cf73bb38423d9ffd54798c5123c9f32a78
Carles Mateu <carlesm@carlesm.com>
2015-03-01 13:15:55 +0100

    Added Donor

--- DjangoTut/sobres/isobres/models.py
+++ DjangoTut/sobres/isobres/models.py
@@ -7,3 +7,8 @@ class Sobre(models.Model):
     date = models.DateTimeField()
     amount = models.IntegerField()
     concept = models.TextField(max_length=100)
+
+
+class Donor(models.Model):
+    name = models.CharField(max_length=40)
+
