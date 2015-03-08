46fb57f3b98455f3991135631dc4d50d409d716b
Carles Mateu <carlesm@carlesm.com>
2015-03-08 11:27:28 +0100

    Added __unicode__ for admin interface

--- DjangoTut/sobres/isobres/models.py
+++ DjangoTut/sobres/isobres/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Donor(models.Model):
     name = models.CharField(max_length=40)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Sobre(models.Model):
     date = models.DateTimeField()
@@ -14,4 +17,7 @@ class Sobre(models.Model):
     concept = models.TextField(max_length=100)
     donor = models.ForeignKey(Donor)
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.donor.name+" - "+self.concept
 
