cef41f664915a19f5b6664a8a58153e8337d5da2
Carles Mateu <carlesm@carlesm.com>
2018-03-05 22:35:51 +0100

    Added two basic models

--- Deploy/tapapp/tapesbars/models.py
+++ Deploy/tapapp/tapesbars/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Bar(models.Model):
+    name = models.CharField(max_length=80)
+    addr = models.TextField(max_length=100)
+
+    def __unicode__(self):
+        return self.name+" "+self.addr
+
+
+class Tapes(models.Model):
+    """Tapes"""
+    name = models.CharField(max_length=80)
+    price = models.IntegerField()
+    bar = models.ForeignKey(Bar)
+
+    def __unicode__(self):
+        return self.bar.name+"-"+self.name
