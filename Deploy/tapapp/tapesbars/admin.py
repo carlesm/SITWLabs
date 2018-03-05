79b5c8adb4e01da449ba6ce4bebd5b38f00f26f4
Carles Mateu <carlesm@carlesm.com>
2018-03-05 22:38:37 +0100

    Fixed imports

--- Deploy/tapapp/tapesbars/admin.py
+++ Deploy/tapapp/tapesbars/admin.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Bar, Tapes
+
 # Register your models here.
 admin.site.register(Bar)
 admin.site.register(Tapes)
